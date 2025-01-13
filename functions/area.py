3d48b6367d967d443097fb116c7fdb700e786674
catediana <catherinenanyala@gmail.com>
2025-01-13 14:18:09 -0800

    created three files area,cheker and greet.py

---
+++ functions/area.py
@@ -0,0 +1,6 @@
+def area_rectangle(length, width):
+    area = length * width
+    return area
+results = area_rectangle(5, 10)
+print(results)
+
---
+++ functions/cheker.py
@@ -0,0 +1,8 @@
+# prompting the user to enter a number
+def check_even_or_odd(number):
+    
+    if number % 2 == 0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+check_even_or_odd(10)
